62f1522b493d854873082fa6d2c83f2c12952730
telierlabs <zixksuper@gmail.com>
2026-02-02 06:36:36 +0700

    Add Login page route to App component

---
+++ App.tsx
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { NewsArticle } from './types';
+import { INITIAL_ARTICLES } from './constants';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Admin from './pages/Admin';
+import NewsDetail from './pages/NewsDetail';
+import Login from './pages/Login'; // TAMBAH INI
+
+const App: React.FC = () => {
+  const [articles, setArticles] = useState<NewsArticle[]>(() => {
+    const saved = localStorage.getItem('telier_articles');
+    return saved ? JSON.parse(saved) : INITIAL_ARTICLES;
+  });
+
+  useEffect(() => {
+    localStorage.setItem('telier_articles', JSON.stringify(articles));
+  }, [articles]);
+
+  const handleAddArticle = (article: NewsArticle) => {
+    setArticles([article, ...articles]);
+  };
+
+  const handleDeleteArticle = (id: string) => {
+    setArticles(articles.filter(a => a.id !== id));
+  };
+
+  return (
+    <div className="min-h-screen flex flex-col font-sans">
+      <Header />
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Home articles={articles} />} />
+          <Route path="/login" element={<Login />} /> {/* TAMBAH INI */}
+          <Route 
+            path="/admin" 
+            element={
+              <Admin 
+                articles={articles} 
+                onAddArticle={handleAddArticle} 
+                onDeleteArticle={handleDeleteArticle} 
+              />
+            } 
+          />
+          <Route path="/news/:id" element={<NewsDetail articles={articles} />} />
+          <Route 
+            path="/category/:cat" 
+            element={<Home articles={articles} />}
+          />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
